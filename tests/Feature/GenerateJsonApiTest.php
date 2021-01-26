f94ee6a81cc0cb637247d2bfa988ca4b46febfcb
Eugeny Nosenko <info@coders.studio>
2021-01-26 12:06:42 +0300

    alpha

--- tests/Feature/GenerateJsonApiTest.php
+++ tests/Feature/GenerateJsonApiTest.php
@@ -14,11 +14,14 @@ class GenerateJsonApiTest extends TestCase
      */
     public function testGenerate()
     {
-        $this->artisan('jsonApi:generate', ['table' => 'posts', '--force' => 1]);
+        $this->artisan('jsonApi:generate', ['table' => 'posts', '--force' => 0]);
         $this->assertFileExists(app_path("Http/Controllers/Api/V1/").'PostController.php');
         $this->assertFileExists(app_path("Http/Resources/Api/V1/Post/").'PostIdentifierResource.php');
         $this->assertFileExists(app_path("Http/Resources/Api/V1/Post/").'PostRelationshipResource.php');
         $this->assertFileExists(app_path("Http/Resources/Api/V1/Post/").'PostResource.php');
         $this->assertFileExists(app_path("Http/Resources/Api/V1/Post/").'PostsResource.php');
+        $this->assertFileExists(app_path("Http/Requests/Api/V1/Post/").'IndexRequest.php');
+        $this->assertFileExists(app_path("Http/Requests/Api/V1/Post/").'StoreRequest.php');
+        $this->assertFileExists(app_path("Http/Requests/Api/V1/Post/").'UpdateRequest.php');
     }
 }
