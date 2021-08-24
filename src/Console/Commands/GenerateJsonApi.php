5cb861f6fb308deab10b44f0715f52efd05c96ac
Eugeny Nosenko <Imnosok@gmail.com>
2021-08-24 18:06:28 +0300

    Update GenerateJsonApi.php

--- src/Console/Commands/GenerateJsonApi.php
+++ src/Console/Commands/GenerateJsonApi.php
@@ -244,7 +244,7 @@ class GenerateJsonApi extends Command
 
         $routes = [
             "Route::pattern('{$modelNameLowerCase}', '[0-9]+');",
-            "Route::resource('" . $route . "', 'Api" . $namespacePath . "\\". $modelName . "Controller', ['except'=> ['edit', 'create']]);",
+            "Route::resource('" . $route . "', \App\Http\Controllers\Api" . $namespacePath . "\\". $modelName . "Controller::class, ['except'=> ['edit', 'create']]);",
         ];
 
         foreach ($routes as $route) {
